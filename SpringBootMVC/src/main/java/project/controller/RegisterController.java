a4715c1563e931026480d7e42a5d5b2938fe9f15
Alexander Þór Hafþórsson <athh15@tg-dw060.rhi.hi.is>
2018-11-08 11:24:53 +0000

    register komið, laga login

--- SpringBootMVC/src/main/java/project/controller/RegisterController.java
+++ SpringBootMVC/src/main/java/project/controller/RegisterController.java
@@ -2,6 +2,7 @@ package project.controller;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,6 +15,12 @@ import project.service.UserService;
 public class RegisterController {
 
     private UserService userService;
+
+    @Autowired
+    public RegisterController(UserService userService){
+        this.userService = userService;
+    }
+
 
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
