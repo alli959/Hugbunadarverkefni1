323c9d1eb1806bcb41b9e62fd3bd4352e1f0813a
Alexander Þór Hafþórsson <athh15@vr2t-dw264.rhi.hi.is>
2018-11-21 09:03:56 +0000

    lagað smá

--- SpringBootMVC/src/main/java/project/controller/EventController.java
+++ SpringBootMVC/src/main/java/project/controller/EventController.java
@@ -34,8 +34,8 @@ public class EventController {
         return "Game";
     }
 
-    @RequestMapping(value = "/alert", method = RequestMethod.GET)
-    public void alert(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    @RequestMapping(value = "/2pt", method = RequestMethod.GET)
+    public void twoPT(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         PrintWriter out = response.getWriter();
         String player1 = "'Alex'";
 
@@ -44,7 +44,7 @@ public class EventController {
         out.println("<script src='https://unpkg.com/sweetalert/dist/sweetalert.min.js'></script>");
         out.println("<script>");
         out.println("$(document).ready(function() {");
-        out.println("swal({closeOnClickOutside: false,title: 'Shot',buttons: " +
+        out.println("swal({closeOnClickOutside: false,title: 'Two point shot',buttons: " +
                 "{Made: {" +
                 "text: 'Made', value: 'made' }, " +
                 "Miss: {" +
@@ -62,10 +62,92 @@ public class EventController {
         out.println("case 'miss':");
         out.println("swal({closeOnClickOutside: false,");
         out.println("title: 'Rebound', buttons: {");
-        out.println("Player1: {text: 'Player 1', value: 'player1'}, Player2: {text: 'Player 2', value: 'player2'}");
+        out.println("Offensive: {text: 'Offensive rebound', value: 'offence'}, Defensive: {text: 'Defensive rebound', value: 'defence'}");
+        out.println("}");
+        out.println("}).then((value) => {");
+        out.println("switch (value) {");
+        out.println("case 'offence':");
+        out.println("swal({closeOnClickOutside: false,");
+        out.println("title: 'Rebound for offence', buttons: {");
+        out.println("Player1: {text: "+ player1 + ", value: 'player1'}, Player2: {text: 'Player 2', value: 'player2'}");
         out.println("}");
         out.println("});");
-        out.println("");
+        out.println("console.log(value);");
+        out.println("break;");
+        out.println("case 'defence':");
+        out.println("swal({closeOnClickOutside: false,");
+        out.println("title: 'Rebound for defence', buttons: {");
+        out.println("Player1: {text: "+ player1 + ", value: 'player1'}, Player2: {text: 'Player 2', value: 'player2'}");
+        out.println("}");
+        out.println("});");
+        out.println("break;");
+        out.println("default:");
+        out.println("break;");
+        out.println("}");
+        out.println("});");
+        out.println("default:");
+        out.println("break;");
+        out.println("}");
+        out.println("})");
+        out.println("});");
+        out.println("</script>");
+
+        RequestDispatcher rd = request.getRequestDispatcher("Game");
+        rd.include(request, response);
+
+    }
+
+    @RequestMapping(value = "/3pt", method = RequestMethod.GET)
+    public void threePT(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        PrintWriter out = response.getWriter();
+        String player1 = "'Alex'";
+
+        out.println("<script src='https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.4/sweetalert2.all.js'></script>");
+        out.println("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>");
+        out.println("<script src='https://unpkg.com/sweetalert/dist/sweetalert.min.js'></script>");
+        out.println("<script>");
+        out.println("$(document).ready(function() {");
+        out.println("swal({closeOnClickOutside: false,title: 'Three point shot',buttons: " +
+                "{Made: {" +
+                "text: 'Made', value: 'made' }, " +
+                "Miss: {" +
+                "text: 'Miss', value: 'miss'}");
+        out.println("}}).then((value) => {");
+        out.println("switch (value) {");
+        out.println("case 'made':");
+        out.println("swal({closeOnClickOutside: false,");
+        out.println("title: 'Assist', buttons: {");
+        out.println("Player1: {text: "+ player1 + ", value: 'player1'}, Player2: {text: 'Player 2', value: 'player2'}");
+        out.println("}");
+        out.println("});");
+        out.println("console.log(value);");
+        out.println("break;");
+        out.println("case 'miss':");
+        out.println("swal({closeOnClickOutside: false,");
+        out.println("title: 'Rebound', buttons: {");
+        out.println("Offensive: {text: 'Offensive rebound', value: 'offence'}, Defensive: {text: 'Defensive rebound', value: 'defence'}");
+        out.println("}");
+        out.println("}).then((value) => {");
+        out.println("switch (value) {");
+        out.println("case 'offence':");
+        out.println("swal({closeOnClickOutside: false,");
+        out.println("title: 'Rebound for offence', buttons: {");
+        out.println("Player1: {text: "+ player1 + ", value: 'player1'}, Player2: {text: 'Player 2', value: 'player2'}");
+        out.println("}");
+        out.println("});");
+        out.println("console.log(value);");
+        out.println("break;");
+        out.println("case 'defence':");
+        out.println("swal({closeOnClickOutside: false,");
+        out.println("title: 'Rebound for defence', buttons: {");
+        out.println("Player1: {text: "+ player1 + ", value: 'player1'}, Player2: {text: 'Player 2', value: 'player2'}");
+        out.println("}");
+        out.println("});");
+        out.println("break;");
+        out.println("default:");
+        out.println("break;");
+        out.println("}");
+        out.println("});");
         out.println("default:");
         out.println("break;");
         out.println("}");
